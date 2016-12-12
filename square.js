a5397d11bc0fc8215971bab7820bd8e8d4ec448b
JackBrasesco <jacbras@nuevaschool.org>
2016-12-12 14:42:36 -0800

    Classadoo autocommit

--- square.js
+++ square.js
@@ -2,7 +2,9 @@
 
 var player = $("#player");
 
-var playerX = Math.floor(Math.random() 
+var playerX = Math.floor(Math.random() * 1080);
+
+var playerY = Math.floor(Math.random() 
 
 $(document).keyDown(function(keyPress) {
   if (keyPress.keyCode = 68) {
