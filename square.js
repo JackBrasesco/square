0bda30ebff55c127fc25fabfa500623866ee0a6c
JackBrasesco <jacbras@nuevaschool.org>
2016-12-12 15:24:13 -0800

    Classadoo autocommit

--- square.js
+++ square.js
@@ -1,34 +1,6 @@
 //PLAYER MOVEMENT 
 
-var player = $("#player");
-
-var playerX = Math.floor(Math.random() * 500);
-
-var playerY = Math.floor(Math.random() * 500);
-
-player.css("left", parseInt(playerX));
-
-player.css("top", playerY);
-
-$(document).keydown(function(keyPress) {
-  if (keyPress.keyCode == 68) {
-    player.css("left", playerX + 10)
-    playerX = playerX + 10
-  }
-  if (keyPress.keyCode == 65) {
-    player.css("left", playerX - 10);
-    playerX = playerX - 10;
-  }
-  if (keyPress.keyCode == 87) {
-    player.css("top", playerY - 10);
-    playerY = playerY - 10;
-  }
-  if (keyPress.keyCode == 83) {
-    player.css("top", playerY + 10);
-    playerY = (playerY + 10);
-  }
-})
-
+var player = new Sprite(50,50,
 //FOOD
 
 var food = document.getElementById("food");
