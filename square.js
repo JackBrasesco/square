3e20ec0a59ff209851e7f77a9415c9e40c2e5f55
JackBrasesco <jacbras@nuevaschool.org>
2016-12-12 15:26:30 -0800

    Classadoo autocommit

--- square.js
+++ square.js
@@ -1,6 +1,22 @@
+//SPRITE DUDE
+
+function Sprite(x,y,w,h,color){
+  this.x=x;
+  this.y=y;
+  this.w=w;
+  this.h=h;
+  this.color=color ? color : "#000";
+}
+Sprite.prototype.drawMe = function (ctx,dx,dy){
+  ctx.fillStyle = this.color;
+  dx= dx ? dx : 0;
+  dy= dy ? dy : 0;
+  ctx.fillRect(this.x+dx,this.y+dy,this.w,this.h);
+}
+
 //PLAYER MOVEMENT 
 
-var player = new Sprite(Math.floor(Math.random() * 800),50,50,50,red)
+var player = new Sprite(Math.floor(Math.random() * 800),Math.floor(Math.random() * 800),50,50,red)
 //FOOD
 
 var food = document.getElementById("food");
