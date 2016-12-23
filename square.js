6119f8dc2b23fe722a1a5d8ae8de795e0ecbc655
JackBrasesco <jacbras@nuevaschool.org>
2016-12-22 20:53:44 -0800

    Classadoo autocommit

--- square.js
+++ square.js
@@ -1,8 +1,39 @@
-//SPRITE DUDE
+//SPRITE FOR PLAYER CANVAS
 
 var food = document.getElementById("food");
 
 var ctx = food.getContext("2d");
+
+var playerX = Math.floor(Math.random() * 800);
+var playerY = Math.floor(Math.random() * 800);
+
+function Sprite(x,y,w,h,color){
+  this.x=x;
+  this.y=y;
+  this.w=w;
+  this.h=h;
+  this.color=color ? color : "#000";
+}
+Sprite.prototype.drawMe = function (ctx,dx,dy){
+  ctx.fillStyle = this.color;
+  dx= dx ? dx : 0;
+  dy= dy ? dy : 0;
+  ctx.beginPath();
+  ctx.moveTo(this.x,this.y);
+  ctx.lineTo(this.x, this.y + this.h);
+  ctx.lineTo(this.x + this.w, this.y + this.h);
+  ctx.lineTo(this.x + this.w, this.y);
+  ctx.lineTo(this.x,this.y)
+  ctx.lineWidth = 5
+  ctx.stroke();
+  ctx.fillRect(this.x+dx,this.y+dy,this.w,this.h);
+}
+
+//SPRITE FOR FOOD CANVAS
+
+var foof = document.getElementById("food2");
+
+var ctx2 = foof.getContext("2d");
 
 var playerX = Math.floor(Math.random() * 800);
 var playerY = Math.floor(Math.random() * 800);
